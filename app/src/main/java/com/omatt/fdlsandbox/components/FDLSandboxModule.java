42d7c71608fdc3d5949338644a468a7a1c6f6adb
Omar Matthew Reyes <omarmatthew@omarmatthew-dpg1.mnl.corp.google.com>
2017-06-24 17:37:20 +0800

    Updated Manifest file, AppModule

--- app/src/main/java/com/omatt/fdlsandbox/components/FDLSandboxModule.java
+++ app/src/main/java/com/omatt/fdlsandbox/components/FDLSandboxModule.java
@@ -1,8 +1,12 @@
 package com.omatt.fdlsandbox.components;
 
+import com.google.firebase.dynamiclinks.FirebaseDynamicLinks;
 import com.omatt.fdlsandbox.utils.AppController;
 
+import javax.inject.Singleton;
+
 import dagger.Module;
+import dagger.Provides;
 
 /**
  * Created by omarmatthew on 6/24/17.
@@ -12,5 +16,15 @@ class FDLSandboxModule {
     private final AppController app;
     public FDLSandboxModule(AppController app) {
         this.app = app;
+    }
+
+    /**
+     * Provide injectable FirebaseAuth instance
+     * @return FirebaseAuth
+     */
+    @Provides
+    @Singleton
+    FirebaseDynamicLinks provideFirebaseDynamicLinks(){
+        return FirebaseDynamicLinks.getInstance();
     }
 }
