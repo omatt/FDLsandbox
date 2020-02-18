d0deb9b0cadba83c0faa67a7fbc7157f1c861912
Omar Matthew <omatt@users.noreply.github.com>
2020-02-18 12:07:14 +0800

    Update access_token_generator.js
    Added comments on functions. Updated var google to var {google}

--- fdl-analytics/access_token_generator.js
+++ fdl-analytics/access_token_generator.js
@@ -5,15 +5,19 @@
 */
 
 var admin = require("firebase-admin");
-var google = require('googleapis');
+var {google} = require('googleapis');
 
+// Load the service account key JSON file
 var serviceAccount = require("LOCATION OF YOUR PRIVATE KEY");
 var credential = admin.credential.cert(serviceAccount);
 
+// Autenticate JWT client with the service account
+// Required scope is https://www.googleapis.com/auth/firebase
 var jwtClient = new google.auth.JWT(serviceAccount.client_email, null, serviceAccount.private_key, [
  'https://www.googleapis.com/auth/firebase'
 ]);
 
+// Use JWT client to generate an access token
 jwtClient.authorize(function(err, tokens) {
   console.log("Token " + tokens.access_token)
 });
